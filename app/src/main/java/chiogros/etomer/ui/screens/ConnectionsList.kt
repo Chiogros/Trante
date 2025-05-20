abacacca3aa60d162902ee2b6af5b5010bad7ccd
Chiogros <0q8hnjtu2@mozmail.com>
2025-05-20 16:03:28 +0200

    style: apply Material theme

--- app/src/main/java/chiogros/etomer/ui/screens/ConnectionsList.kt
+++ app/src/main/java/chiogros/etomer/ui/screens/ConnectionsList.kt
@@ -4,13 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -23,8 +23,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import chiogros.etomer.R
 
@@ -63,25 +66,24 @@ fun Fab(onClick: () -> Unit) {
     ExtendedFloatingActionButton(
         onClick = onClick
     ) {
-        Row {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
                 imageVector = Icons.Sharp.Add,
                 contentDescription = stringResource(R.string.create_connection)
             )
-            Text( text = stringResource(R.string.create_connection) )
+            Text(text = stringResource(R.string.create_connection))
         }
     }
 }
 
 @Composable
 fun Item() {
-    Row (modifier = Modifier.fillMaxWidth().height(height = 72.dp).padding(16.dp)) {
-        Text(modifier = Modifier.weight(1F), text = "Type")
+    Row (modifier = Modifier.fillMaxWidth().padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+        Text(modifier = Modifier.weight(1F), text = "Type", fontWeight = FontWeight.Normal, fontFamily = FontFamily.Monospace)
         Column(modifier = Modifier.weight(3F)) {
-            Text("User: user")
-            Text("example.net")
+            Text(text = "example.net", style = MaterialTheme.typography.bodyLarge, fontFamily = FontFamily.Monospace)
+            Text(text = "User: user", style = MaterialTheme.typography.bodyMedium)
         }
-
         var checked by remember { mutableStateOf(true) }
         Switch(
             checked = checked,
@@ -89,4 +91,5 @@ fun Item() {
             modifier = Modifier.weight(1F)
         )
     }
+    HorizontalDivider(color = MaterialTheme.colorScheme.outlineVariant, thickness = 1.dp)
 }
