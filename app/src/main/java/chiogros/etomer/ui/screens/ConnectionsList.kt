22c98cecfd80bf7433710ce3f7911080b7649200
Chiogros <0q8hnjtu2@mozmail.com>
2025-05-20 08:41:50 +0200

    feat: add Items in list

--- app/src/main/java/chiogros/etomer/ui/screens/ConnectionsList.kt
+++ app/src/main/java/chiogros/etomer/ui/screens/ConnectionsList.kt
@@ -1,7 +1,12 @@
 package chiogros.etomer.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,12 +14,18 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import chiogros.etomer.R
 
 @Composable
@@ -23,7 +34,13 @@ fun ConnectionsList(onClick: () -> Unit) {
         modifier = Modifier.fillMaxSize(),
         topBar = { TopBar() },
         floatingActionButton = { Fab(onClick) },
-    ) { innerPadding -> ( innerPadding ) }
+    ) { innerPadding ->
+        LazyColumn(modifier = Modifier.padding(innerPadding)) {
+            items(10) { index ->
+                Item()
+            }
+        }
+    }
 }
 
 
@@ -53,5 +70,23 @@ fun Fab(onClick: () -> Unit) {
             )
             Text( text = stringResource(R.string.create_connection) )
         }
+    }
+}
+
+@Composable
+fun Item() {
+    Row (modifier = Modifier.fillMaxWidth().height(height = 72.dp).padding(16.dp)) {
+        Text(modifier = Modifier.weight(1F), text = "Type")
+        Column(modifier = Modifier.weight(3F)) {
+            Text("User: user")
+            Text("example.net")
+        }
+
+        var checked by remember { mutableStateOf(true) }
+        Switch(
+            checked = checked,
+            onCheckedChange = { checked = it },
+            modifier = Modifier.weight(1F)
+        )
     }
 }
