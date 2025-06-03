8336c8f4f05c9526a33e1771f59f23c316791a17
Chiogros <0q8hnjtu2@mozmail.com>
2025-06-03 23:06:04 +0200

    feat: implement best practice's state use

--- app/src/main/java/chiogros/etomer/ui/state/ConnectionEditViewModel.kt
+++ app/src/main/java/chiogros/etomer/ui/state/ConnectionEditViewModel.kt
@@ -7,23 +7,52 @@ import chiogros.etomer.data.storage.ConnectionSftp
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 data class ConnectionEditUiState(
-    var host: String,
-    var user: String
-)
+    var host: String = "",
+    var type: String = "",
+    var user: String = ""
+    )
 
 class ConnectionEditViewModel(private val repository: ConnectionSftpRepository) : ViewModel() {
-    private val _uiState = MutableStateFlow(ConnectionEditUiState(
-        host = "",
-        user = ""
-    ))
+    private val _uiState = MutableStateFlow(ConnectionEditUiState())
     val uiState: StateFlow<ConnectionEditUiState> = _uiState.asStateFlow()
+
+    fun refreshAll() {
+        setHost("")
+        setType("")
+        setUser("")
+    }
 
     fun insert(connection: ConnectionSftp) {
         viewModelScope.launch {
             repository.insert(connection)
+        }
+    }
+
+    fun setHost(host: String) {
+        _uiState.update {
+            it.copy(
+                host = host
+            )
+        }
+    }
+
+    fun setType(type: String) {
+        _uiState.update {
+            it.copy(
+                type = type
+            )
+        }
+    }
+
+    fun setUser(user: String) {
+        _uiState.update {
+            it.copy(
+                user = user
+            )
         }
     }
 }
--- app/src/main/java/chiogros/etomer/ui/ui/screens/ConnectionEdit.kt
+++ app/src/main/java/chiogros/etomer/ui/ui/screens/ConnectionEdit.kt
@@ -1,13 +1,18 @@
 package chiogros.etomer.ui.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.sharp.ArrowBack
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -18,13 +23,12 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
 import chiogros.etomer.R
 import chiogros.etomer.data.storage.ConnectionSftp
 import chiogros.etomer.ui.state.ConnectionEditViewModel
@@ -43,7 +47,9 @@ fun ConnectionEdit(onBack: () -> Unit, viewModel: ConnectionEditViewModel) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ConnectionEditTopBar(onClick: () -> Unit, onSave: () -> Unit, viewModel: ConnectionEditViewModel) {
+fun ConnectionEditTopBar(onBack: () -> Unit, onSave: () -> Unit, viewModel: ConnectionEditViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+
     TopAppBar(
         colors = topAppBarColors(
             containerColor = MaterialTheme.colorScheme.background,
@@ -53,7 +59,10 @@ fun ConnectionEditTopBar(onClick: () -> Unit, onSave: () -> Unit, viewModel: Con
             Text(stringResource(R.string.create_connection))
         },
         navigationIcon = {
-            IconButton(onClick = onClick) {
+            IconButton(onClick = {
+                viewModel.refreshAll()
+                onBack()
+            }) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Sharp.ArrowBack,
                     contentDescription = stringResource(R.string.get_back_previous_screen)
@@ -63,7 +72,8 @@ fun ConnectionEditTopBar(onClick: () -> Unit, onSave: () -> Unit, viewModel: Con
         actions = {
             TextButton(
                 onClick = {
-                    viewModel.insert(ConnectionSftp(host = viewModel.uiState.value.host, user = viewModel.uiState.value.user))
+                    viewModel.insert(ConnectionSftp(host = uiState.host, user = uiState.user))
+                    viewModel.refreshAll()
                     onSave()
                 }
             ) {
@@ -75,37 +85,54 @@ fun ConnectionEditTopBar(onClick: () -> Unit, onSave: () -> Unit, viewModel: Con
 
 @Composable
 fun ConnectionEditForm(viewModel: ConnectionEditViewModel) {
-    var host by remember { mutableStateOf("") }
-    var user by remember { mutableStateOf("") }
+    val uiState by viewModel.uiState.collectAsState()
 
-    OutlinedTextField(
-        value = host,
-        onValueChange = {
-            host = it
-            viewModel.uiState.value.host = it
-        },
-        modifier = Modifier.fillMaxWidth(),
-        label = { Text(stringResource(R.string.host)) },
-        placeholder = { Text(stringResource(R.string.example_dot_net)) },
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.None,
-            autoCorrectEnabled = false
-        ),
-        singleLine = true
-    )
+    Row( modifier = Modifier.fillMaxWidth() ) { ConnectionEditTypePicker(viewModel) }
 
-    OutlinedTextField(
-        value = user,
-        onValueChange = {
-            user = it
-            viewModel.uiState.value.user = it
-        },
-        modifier = Modifier.fillMaxWidth(),
-        label = { Text(stringResource(R.string.user)) },
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.None,
-            autoCorrectEnabled = false
-        ),
-        singleLine = true
+    if (uiState.type != "") {
+        OutlinedTextField(
+            value = uiState.host,
+            onValueChange = { viewModel.setHost(it) },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text(stringResource(R.string.host)) },
+            placeholder = { Text(stringResource(R.string.example_dot_net)) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrectEnabled = false
+            ),
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = uiState.user,
+            onValueChange = { viewModel.setUser(it) },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text(stringResource(R.string.user)) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrectEnabled = false
+            ),
+            singleLine = true
+        )
+    }
+}
+
+@Composable
+fun ConnectionEditTypePicker(viewModel: ConnectionEditViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    FilterChip(
+        onClick = { viewModel.setType( if (uiState.type != "SFTP") "SFTP" else "" ) },
+        label = { Text(stringResource(R.string.sftp)) },
+        modifier = Modifier.padding(start = 8.dp),
+        selected = (uiState.type == "SFTP"),
+        leadingIcon =
+            if (uiState.type == "SFTP") {{
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "",
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )}
+            } else { null }
     )
 }
