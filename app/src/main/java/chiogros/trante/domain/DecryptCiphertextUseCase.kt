643c2c1f9c8b21a523849847ecdb357a2aed83b0
Chiogros <0q8hnjtu2@mozmail.com>
2025-09-09 17:39:37 +0200

    fix: remove unused files

--- app/src/main/java/chiogros/trante/domain/DecryptCiphertextUseCase.kt
+++
@@ -1,7 +0,0 @@
-package chiogros.trante.domain
-
-class DecryptCiphertextUseCase {
-    suspend operator fun invoke(cipher: String, iv: String): ByteArray {
-        return ByteArray(0)
-    }
-}
--- app/src/main/java/chiogros/trante/domain/EncryptUseCase.kt
+++
@@ -1,7 +0,0 @@
-package chiogros.trante.domain
-
-class EncryptUseCase {
-    operator fun invoke(cleartext: String): ByteArray {
-        return ByteArray(0)
-    }
-}
