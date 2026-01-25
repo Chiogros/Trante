83a3cfe7e9977f4d7e931ce139807173a50961f8
Chiogros <0q8hnjtu2@mozmail.com>
2026-01-25 18:52:50 +0100

    chore: bump AGP and libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.androidx.room)
     alias(libs.plugins.com.google.devtools.ksp)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     kotlin(libs.plugins.plugin.serialization.get().pluginId).version(libs.versions.serialization)
 }
